6c3da508bf1605b577e3076e8cb4492d1f1dbe5f
Anamul Hassan <itsanamulhassan@gmail.com>
2025-12-20 07:38:21 +0600

    problem<1> 1 Two Sum (optimize)

--- 1_Two_Sum.cpp
+++ 1_Two_Sum.cpp
@@ -1,3 +1,4 @@
+// brute-force solution
 class Solution
 {
 public:
@@ -17,5 +18,27 @@ public:
             }
         }
         return indexs;
+    }
+};
+
+// optimize solution
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int complement = target - nums[i];
+
+            if (map.find(complement) != map.end())
+            {
+                return {i, map[complement]};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
     }
 };
