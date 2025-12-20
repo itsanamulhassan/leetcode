82a61c317d8f74510a5cfb8ee9fa910dcbc84034
Anamul Hassan <itsanamulhassan@gmail.com>
2025-12-20 06:53:40 +0600

    problem<1> 1 Two Sum

---
+++ 1_Two_Sum.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> indexs;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                long sum = nums[i] + nums[j];
+                if (sum == target)
+                {
+                    indexs.push_back(i);
+                    indexs.push_back(j);
+                }
+            }
+        }
+        return indexs;
+    }
+};
